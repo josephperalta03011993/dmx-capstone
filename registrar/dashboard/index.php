ac0cddf08fa8ecaeff9e0e62735938630df41dcb
josephperalta03011993 <joseph.peralta03011993@gmail.com>
2025-03-02 01:59:10 +0800

    add total students with link to dashboard

--- registrar/dashboard/index.php
+++ registrar/dashboard/index.php
@@ -3,18 +3,27 @@
     include_once('../../database/conn.php');
     include_once('../../layouts/header.php');
 
-    // Query to get the total number of students
-    $sql_students = "SELECT COUNT(*) AS total_students FROM students WHERE status = 'reserved'"; 
+    // Query to get the total number of students reserved
+    $sql_students = "SELECT COUNT(*) AS total_students FROM students"; 
     $result_students = $conn->query($sql_students);
     $row_students = $result_students->fetch_assoc();
     $total_students = $row_students['total_students'];
+
+    // Query to get the total number of students 
+    $sql_reserved = "SELECT COUNT(*) AS total_students FROM students WHERE status = 'reserved'"; 
+    $result_reserved = $conn->query($sql_reserved);
+    $row_reserved = $result_reserved->fetch_assoc();
+    $total_reserved = $row_reserved['total_students'];
 ?>
 
 <h2>Registrar Dashboard</h2>
 
 <div id="container">
     <div id="total-reservation-container">
-        <h3><i class="fa-solid fa-graduation-cap"></i> <a href="manage_students.php?status=reserved" id="total-reservation-link">Total Reservation: </a><?php echo $total_students; ?></h3>
+        <h3><i class="fa-solid fa-graduation-cap"></i> <a href="manage_students.php?status=reserved" id="total-reservation-link">Total Reservation: </a><?php echo $total_reserved; ?></h3>
+    </div>
+    <div id="total-reservation-container">
+        <h3><i class="fa-solid fa-graduation-cap"></i> <a href="manage_students.php?status=all" id="total-reservation-link">Total Students: </a><?php echo $total_students; ?></h3>
     </div>
 </div>
 
